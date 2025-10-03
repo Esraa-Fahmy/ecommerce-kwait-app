326251407b36b7a003c9a50fed483e738d3d46fc
esraa <esraaalrassas@gmail.com>
2025-10-04 00:09:19 +0300

    wwwww

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,21 +5,21 @@ const sendEmail = async (options) => {
   // 1) Create transporter 
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: 465, // if secure false port = 587, if true port= 465
+    port: 465,
+        secure: true, // ✅ لازم مع 465
+ // if secure false port = 587, if true port= 465
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
     },
-    tls: {
-    rejectUnauthorized: false, 
-  },
+ 
   });
 
   
 
  // 2) Define email options (like from, to, subject, email content)
  const mailOpts = {
-  from: 'Trendora.com <esraaalrassas@gmail.com>',
+  from: 'تطبيق عروض <esraaalrassas@gmail.com>',
   to: options.email,
   subject: options.subject,
   text: options.message, 
