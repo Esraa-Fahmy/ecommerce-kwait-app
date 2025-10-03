4d07f74f81d183e1b660a7e2f77f7d2296797ef2
esraa <esraaalrassas@gmail.com>
2025-10-04 00:22:02 +0300

    cccc'

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,32 +1,27 @@
 const nodemailer = require('nodemailer');
 
-// Nodemailer
 const sendEmail = async (options) => {
-  // 1) Create transporter 
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: 465,
-        secure: true, // ✅ لازم مع 465
- // if secure false port = 587, if true port= 465
+    port: process.env.EMAIL_PORT,
+    secure: process.env.EMAIL_PORT == 465, 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD,
     },
- 
+    tls: {
+      rejectUnauthorized: false, // ✅ يتجاهل self-signed certificates
+    },
   });
 
-  
+  const mailOpts = {
+    from: `تطبيق عروض <${process.env.EMAIL_USER}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    html: options.html || options.message,
+  };
 
- // 2) Define email options (like from, to, subject, email content)
- const mailOpts = {
-  from: 'تطبيق عروض <esraaalrassas@gmail.com>',
-  to: options.email,
-  subject: options.subject,
-  text: options.message, 
-  html: options.html || options.message, 
-};
-
-  // 3) Send email
   await transporter.sendMail(mailOpts);
 };
 
