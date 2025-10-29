fbc6912411f5eb04900bfda29ca171adb6e86101
esraa <esraaalrassas@gmail.com>
2025-10-29 16:52:37 +0300

    w

--- middlewares/uploadImageMiddleWare.js
+++ middlewares/uploadImageMiddleWare.js
@@ -1,33 +1,24 @@
 const multer = require('multer');
-const path = require('path');
 const ApiError = require('../utils/apiError');
-const fs = require('fs');
 
-// تخزين الصور على الهارد
-const multerStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, '../uploads');
-    if (!fs.existsSync(uploadPath)) fs.mkdirSync(uploadPath, { recursive: true });
-    cb(null, uploadPath);
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const name = file.fieldname + '-' + Date.now() + ext;
-    cb(null, name);
-  }
-});
+const multerOptions = () => {
 
-// فلتر للصور
-const multerFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image')) {
-    cb(null, true);
-  } else {
-    cb(new ApiError('Only Images allowed', 400), false);
-  }
+  const multerStorage = multer.memoryStorage();
+
+  const multerFilter = function (req, file, cb) {
+    if (file.mimetype.startsWith('image')) {
+      cb(null, true);
+    } else {
+      cb(new ApiError('Only Images allowed', 400), false);
+    }
+  };
+
+  const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+
+  return upload;
 };
 
-const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+exports.uploadSingleImage = (fieldName) => multerOptions().single(fieldName);
 
-exports.uploadSingleImage = (fieldName) => upload.single(fieldName);
-
-exports.uploadMixOfImages = (arrayOfFields) => upload.fields(arrayOfFields);
+exports.uploadMixOfImages = (arrayOfFields) =>
+  multerOptions().fields(arrayOfFields); 
