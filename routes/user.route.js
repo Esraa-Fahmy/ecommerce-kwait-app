0c93b379aee7d8e8c5bf0fb8b9b9515e4d9cae38
esraa <esraaalrassas@gmail.com>
2025-11-12 07:13:57 +0200

    ssssssssssssssssssssssssssssssssssssssssssssssssssssss

--- routes/user.route.js
+++ routes/user.route.js
@@ -27,11 +27,12 @@ router.delete('/deleteMyAcc', deleteLoggedUserAccount)
 router.route('/')
 .get( Auth.allowedTo('admin'), getUsers)
 .post( Auth.allowedTo('admin'), uploadUserImage, resizeImage, createUserValidator, createUser);
+router.get("/stats", Auth.allowedTo("admin"), getAppStats);
 router.route('/:id')
 .get( Auth.allowedTo('admin'), getUserValidator, getUserById)
 .put( Auth.allowedTo('admin'), uploadUserImage, resizeImage, updateUserValidator , updateUser)
 .delete( Auth.allowedTo('admin'), deleteUserValidator, deleteUser);
-router.get("/stats", Auth.allowedTo("admin"), getAppStats);
+
 
 
 module.exports = router;  
